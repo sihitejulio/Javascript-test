0503a9cfa1be0b839f45ccd1523f7d7c93cf19ba
Julio Sihite <julio.p.sihite@gmail.com>
2018-10-18 18:13:40 +0700

    test if null and false

--- sum.js
+++ sum.js
@@ -1,5 +1,8 @@
-function sum(a,b){
-    return a+b;
-}
+const sum = {
+    // return a+b;
+    add:(a,b)  => a+b,
+    isNull: () => null,
+    checkCalue:x=>x
+};
 
 module.exports = sum;
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,15 @@
 const sum = require('../sum');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+  expect(sum.add(1, 2)).not.toBe(6);
+});
+
+
+test('If null', () => {
+  expect(sum.isNull()).toBeNull();
+});
+
+
+test('If False', () => {
+  expect(sum.checkCalue(0)).toBeFalsy();
 });
